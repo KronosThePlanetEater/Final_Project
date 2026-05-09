2fa417204af95d91ace0645986f42cf3f2180c1e
KronosThePlanetEater <128649592+KronosThePlanetEater@users.noreply.github.com>
2026-05-09 14:34:30 -0400

    Delete tools/ffmpeg/windows/ffmpeg-8.1-full_build-shared directory

--- tools/ffmpeg/windows/ffmpeg-8.1-full_build-shared/include/libavformat/version.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b0ef6acb296e6b20cffb3b137402fe198e1b5d8e68bf812d07e7820d89e95792
-size 1652
--- tools/ffmpeg/windows/ffmpeg-8.1-full_build-shared/include/libavutil/ffversion.h
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ce212021b188d9be654ed512bdb6ca7f17a90a4d60a955c02ba32c4cebd27ea1
-size 206
